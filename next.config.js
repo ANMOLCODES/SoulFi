a65202ea2e8e3b3a8bd03715e1b34727e8c6ac28
Anmol Arora <mr.anmolarora@outlook.com>
2022-08-28 13:16:26 +0530

    next.config changes for spheron

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ...,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
+
+
